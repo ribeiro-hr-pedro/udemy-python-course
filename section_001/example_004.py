58d482f7836e343be0e6a42f85acf9bcc2b649ce
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-08 16:56:04 -0300

    feat: variables and data types

---
+++ section_001/example_004.py
@@ -0,0 +1,30 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Variables and data types
+# Date: 2024-01-08 - Time: 19:00
+
+number = 2024
+
+print(number)
+
+first_name = 'Pedro'
+last_name = 'Ribeiro'
+
+print(first_name, last_name)
+
+legal_age = 21 >= 18
+
+print('Is he/she of legal age?', legal_age)
+
+first_name = 'Pedro'
+last_name = 'Ribeiro'
+age = 28
+birth_year = 2024 - age
+legal_age = age >= 18
+height = 1.90
+
+print('First name:', first_name)
+print('Last name:', last_name)
+print('Age:', age)
+print('Birth year:', birth_year)
+print('Is he/she of legal age:', legal_age)
+print('Height (m):', height)
