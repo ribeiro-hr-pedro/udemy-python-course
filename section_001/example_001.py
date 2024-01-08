68a61855399c58d252753c03b5d1761f31787592
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-08 16:54:22 -0300

    feat: comments and hello world program

---
+++ section_001/example_001.py
@@ -0,0 +1,19 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Comments and hello world program
+# Date: 2024-01-08 - Time: 18:00
+
+# This is a one line comment in Python
+
+'''
+This is a multi-line comment in Python
+Here we can write more than one comment line
+This is called docstring and is used for documentation
+'''
+
+"""
+This is also a multi-line comment in Python
+Here we can write more than one comment line
+This is called docstring and is used for documentation
+"""
+
+print('Hello world!')
