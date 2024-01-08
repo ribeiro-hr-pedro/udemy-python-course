3d7de4eeb7d0a5031d9cbe9809a58dd4cac64e8f
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-08 16:56:29 -0300

    feat: mathematical operators

---
+++ section_001/example_005.py
@@ -0,0 +1,30 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Mathematical operators
+# Date: 2024-01-08 - Time: 19:20
+
+addition = 10 + 5
+print('Addition:', addition)
+
+subtraction = 10 - 5
+print('Subtraction:', subtraction)
+
+multiplication = 10 * 5
+print('Multiplication:', multiplication)
+
+division = 10 / 3
+print('Division:', division)
+
+integer_division = 10 // 3
+print('Integer division 1:', integer_division)
+
+integer_division = 10 // 3.0
+print('Integer division 2:', integer_division)
+
+exponentiation = 2 ** 3
+print('Exponentiation:', exponentiation)
+
+modulo = 10 % 2
+print('Modulo 1:', modulo)
+
+modulo = 10 % 3
+print('Modulo 2:', modulo)
