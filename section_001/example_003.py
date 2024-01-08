26a74538ab640e1acf3af348b1f9f3fbdb02cafb
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-08 16:55:32 -0300

    feat: introduction to data types

---
+++ section_001/example_003.py
@@ -0,0 +1,39 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Introduction to data types
+# Date: 2024-01-08 - Time: 18:40
+
+print('Pedro Henrique')
+print(type('Pedro Henrique'))
+
+print(2024)
+print(type(2024))
+
+print(-10)
+print(type(-10))
+
+print(10.15)
+print(type(10.15))
+
+print(-20.30)
+print(type(-20.30))
+
+print(True)
+print(type(True))
+
+print(False)
+print(type(False))
+
+print(float(10))
+print(type(float(10)))
+
+print(10 + int('10'))
+print(type(10 + int('10')))
+
+print('20' + str(24))
+print(type('20' + str(24)))
+
+print(bool(1))
+print(type(bool(1)))
+
+print(bool(0))
+print(type(bool(0)))
