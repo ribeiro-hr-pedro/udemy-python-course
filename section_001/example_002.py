97a10a9153d316c2e84eca4e079f641e515a8724
Pedro <ribeiro.hr.pedro@gmail.com>
2024-01-08 16:55:00 -0300

    feat: print function and strings

---
+++ section_001/example_002.py
@@ -0,0 +1,24 @@
+# Student: Pedro Henrique Resende Ribeiro
+# Description: Print function and strings
+# Date: 2024-01-08 - Time: 18:20
+
+print(123)
+
+print(1, 2, 3)
+
+print(1, 2, 3, sep='-')
+
+print(1, 2, sep='-', end='*')
+print(3, 4, sep='+', end='\n')
+
+# /r/n = Carriage Return and Line Feed (CRLF)
+
+print('Pedro Henrique')
+print(type('Pedro Henrique'))
+
+print("Pedro Henrique")
+print(type("Pedro Henrique"))
+
+print('Example quote: "To be or not to be, that is the question"')
+print("Example quote: 'To be or not to be, that is the question'")
+print('Example quote: \'To be or not to be, that is the question\'')
